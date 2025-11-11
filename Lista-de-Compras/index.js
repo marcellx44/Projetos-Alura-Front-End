30678abd57649ce189937aeb215bafa246db2b6f
Marcella Oliveira <marcella.oliveira.diasss@gmail.com>
2025-11-11 11:31:59 -0300

    Função de adicionar criada.

--- Lista-de-Compras/index.js
+++ Lista-de-Compras/index.js
@@ -1,3 +1,29 @@
-const inputItem = document.getElementById("input-item")
-
-console.log(inputItem.value);
+//Captura o valor ao clicar em salvar item
+var btn_salvar= document.getElementById("adicionar-item");
+btn_salvar.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    var valor = document.getElementById("input-item").value;
+    if(valor===""){
+        console.log("Vazio");
+        return
+    } else{
+        var lista= document.getElementById("lista-de-compras");
+        var elemento = document.createElement("li");      
+        var item_novo= document.createElement("div");
+        item_novo.classList.add("lista-item-container");
+        var checkbox= document.createElement("input")
+        checkbox.type="checkbox";
+        let contador=1;
+        contador++;
+        checkbox.id="checkbox-"+ contador;
+        
+        var nomeItem= document.createElement("p");
+        nomeItem.innerText=valor;
+        
+        lista.appendChild(elemento);
+        elemento.appendChild(item_novo);
+        item_novo.appendChild(checkbox);
+        item_novo.appendChild(nomeItem);
+        
+    }
+})
