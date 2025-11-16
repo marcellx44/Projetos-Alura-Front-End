fdd936efb894c66bd6d8633d153b4c32cb8ad431
Marcella <marcella.oliveira.diasss@gmail.com>
2025-11-16 17:19:09 -0300

    Modularização da função CriarItem

--- Lista-de-Compras/index.js
+++ Lista-de-Compras/index.js
@@ -1,62 +1,28 @@
+import { criarItem } from "./modules/CriarItem.js";
 //Mensagem inicial de lista vazia
-var lista = document.querySelector(".container-lista");
+const lista_vazia = document.querySelector(".container-lista");
 var mensagem_vazia= document.createElement("p");
 mensagem_vazia.innerText="A lista ainda está vazia.";
-lista.appendChild(mensagem_vazia);
-var lista_vazia=true;
+lista_vazia.appendChild(mensagem_vazia);
+var vazia=true;
 
-//criação da função de salvar item
+var lista= document.getElementById("lista-de-compras");
 var btn_salvar= document.getElementById("adicionar-item");
 btn_salvar.addEventListener("click", (evento)=>{
     evento.preventDefault();
-    var valor = document.getElementById("input-item").value;
-    //verifica se o usuário salvou um item vazio
+    var valor= document.getElementById("input-item").value;
     if(valor===""){
-        console.log("Vazio");
-        return
-    } else{
-        var lista= document.getElementById("lista-de-compras");
-        var elemento = document.createElement("li");      
-        var item_novo= document.createElement("div");
-        item_novo.classList.add("lista-item-container");
-        var checkbox= document.createElement("input")
-        checkbox.type="checkbox";
-        let contador=1;
-        contador++;
-        checkbox.id="checkbox-"+ contador;
-        
-        var nomeItem= document.createElement("p");
-        nomeItem.innerText=valor;
-
-        //evento que troca a decoração do item quando é marcado
-        checkbox.addEventListener("click", ()=>{            
-            if(checkbox.checked){
-                nomeItem.style.textDecoration="line-through";
-            } else{
-                nomeItem.style.textDecoration="none";
-            }
-        })
-
-        //conexões do DOM
-        lista.appendChild(elemento);
-        elemento.appendChild(item_novo);
-        item_novo.appendChild(checkbox);
-        item_novo.appendChild(nomeItem);
-
-        //Inserindo data e hora
-
-        const dia_semana= new Date().toLocaleDateString("pt-BR", {weekday: "long", day: "numeric", month:"2-digit", year: "numeric", hour:"2-digit", minute:"2-digit"} );
-        console.log(dia_semana);
-        
-        var data = document.createElement("p");
-        data.classList.add("texto-data");
-        data.innerText=dia_semana;
-        elemento.appendChild(data);
-        lista_vazia=false;
-        //verificar se lista está vazia 
-       if(lista_vazia===false){
-        mensagem_vazia.style.display="none";
-       }
-       
+        console.log("vazio");
+    }else{
+        var novo_item= criarItem(valor);
+        lista.appendChild(novo_item);
+        vazia=false;
+        //verificação da lista vazia;
+        if(vazia===false){
+            mensagem_vazia.style.display="none";
+        }
     }
+
 })
+
+console.log(vazia);
---
+++ Lista-de-Compras/modules/CriarItem.js
@@ -0,0 +1,40 @@
+export function criarItem(valor){
+        var elemento = document.createElement("li");      
+        var item_novo= document.createElement("div");
+        item_novo.classList.add("lista-item-container");
+        var checkbox= document.createElement("input")
+        checkbox.type="checkbox";
+        let contador=1;
+        contador++;
+        checkbox.id="checkbox-"+ contador;
+        
+        var nomeItem= document.createElement("p");
+        nomeItem.innerText=valor;
+
+        //evento que troca a decoração do item quando é marcado
+        checkbox.addEventListener("click", ()=>{            
+            if(checkbox.checked){
+                nomeItem.style.textDecoration="line-through";
+            } else{
+                nomeItem.style.textDecoration="none";
+            }
+        })
+
+        //conexões do DOM
+
+        elemento.appendChild(item_novo);
+        item_novo.appendChild(checkbox);
+        item_novo.appendChild(nomeItem);
+
+        //Inserindo data e hora
+
+        const dia_semana= new Date().toLocaleDateString("pt-BR", {weekday: "long", day: "numeric", month:"2-digit", year: "numeric", hour:"2-digit", minute:"2-digit"} );
+        console.log(dia_semana);
+        
+        var data = document.createElement("p");
+        data.classList.add("texto-data");
+        data.innerText=dia_semana;
+        elemento.appendChild(data);
+        
+        return elemento;
+    }
