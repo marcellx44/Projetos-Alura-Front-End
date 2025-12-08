f0f5813a32dd4857bd8c5989bc6955ba916a6533
Marcella Oliveira <marcella.oliveira.diasss@gmail.com>
2025-12-08 15:36:51 -0300

    Usuário pode adicionar um valor ao digitar na lista

--- React/ReactHooks/useState/useState-studies/src/App.jsx
+++ React/ReactHooks/useState/useState-studies/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 
-const initialTodos = [{id:0,text:'item 1'},{id:1,text:'item 2'},{id:2,text:'item 3'}];
-
 export default function TodoList() {
+  const initialTodos = [{id:0,text:'item 1'},{id:1,text:'item 2'},{id:2,text:'item 3'}];
   const [todos, setTodos] = useState(initialTodos);
-  const value= 'teste'
+  let valorNovo=''
 
   return (
     <>
+      <input onChange={(elemento) =>{
+        valorNovo=elemento.target.value
+        console.log(valorNovo);
+      }}
+        type="text"
+       />
       <button onClick={() => {
-        setTodos([...todos, {id: todos.length, text:value}]);
+        setTodos([...todos, {id: todos.length, text:valorNovo}]);
       }}>Add</button>
       <ul>
         {todos.map(item => (
