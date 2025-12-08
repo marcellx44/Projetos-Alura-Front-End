cb3eb7b6fdc99d17aabbe76e76d9a4d9bb37cad9
Marcella Oliveira <marcella.oliveira.diasss@gmail.com>
2025-12-08 15:47:39 -0300

    usando useState para controlar o input

--- React/ReactHooks/useState/useState-studies/src/App.jsx
+++ React/ReactHooks/useState/useState-studies/src/App.jsx
@@ -3,18 +3,19 @@ import { useState } from 'react';
 export default function TodoList() {
   const initialTodos = [{id:0,text:'item 1'},{id:1,text:'item 2'},{id:2,text:'item 3'}];
   const [todos, setTodos] = useState(initialTodos);
-  let valorNovo=''
+  const [valorAtual, setValorAtual] = useState('');
+  // let valorNovo=''
 
   return (
     <>
       <input onChange={(elemento) =>{
-        valorNovo=elemento.target.value
-        console.log(valorNovo);
+        setValorAtual(elemento.target.value)
+        console.log(valorAtual);
       }}
         type="text"
        />
       <button onClick={() => {
-        setTodos([...todos, {id: todos.length, text:valorNovo}]);
+        setTodos([...todos, {id: todos.length, text:valorAtual}]);
       }}>Add</button>
       <ul>
         {todos.map(item => (
